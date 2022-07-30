2d73f3b7cace39f1b8cb8a444f5f8f9ccfb39c95
helionext <juniorjobs.zen@gmail.com>
2022-07-29 22:28:03 -0300

    adicionando info de echo

--- script-iac.sh
+++ script-iac.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Atualizando o servicor..."
 apt-get install update
 apt-get install upgrade -y
 apt-get install unzip -y
 apt-get install apache2 -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
 
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
@@ -12,4 +15,4 @@ rm /var/www/html/index.html
 
 unzip main.zip
 cd linux-site-dio-main 
-cp -R * /var/www/html
+cp -R * /var/www/html/
